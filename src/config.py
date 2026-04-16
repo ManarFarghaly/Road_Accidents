90871b5183b68e81f8b037638b0b1a2021997cfd
ManarFarghaly <manar.abdulshafi03@eng-st.cu.edu.eg>
2026-04-16 22:42:11 +0200

    update config

--- src/config.py
+++ src/config.py
@@ -55,11 +55,6 @@ def get_spark(app_name: str = "road-accidents") -> SparkSession:
         .config("spark.sql.execution.arrow.pyspark.enabled", "true")
         .config("spark.hadoop.fs.file.impl", "org.apache.hadoop.fs.LocalFileSystem")
         .config("spark.hadoop.fs.file.impl.disable.cache", "true")
-            # 🚨 CRITICAL FIX: disable Hadoop native layer
-    .config("spark.hadoop.fs.file.impl", "org.apache.hadoop.fs.LocalFileSystem")
-
-    # 🚨 CRITICAL FIX: force safe file handling in Spark 4
     .config("spark.hadoop.mapreduce.fileoutputcommitter.algorithm.version", "2")
-
         .getOrCreate()
     )
